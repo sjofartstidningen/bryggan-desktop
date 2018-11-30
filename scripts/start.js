d728e2847dbc361c20b7286eee2aa16537b2f5fc
Adam Bergman <adam@fransvilhelm.com>
2018-11-30 17:10:18 +0100

    chore: Simplify watch script

--- scripts/start.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const { spawn } = require('child_process');
-
-const cp = spawn('electron', ['.'], {
-  cwd: path.join(__dirname, '..'),
-  stdio: [process.stdin, process.stdout, process.stderr],
-});
-
-process.on('exit', () => cp.kill());
