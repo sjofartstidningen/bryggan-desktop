2aaa1908a1ad34ce894cf88f745ae4d4d45d398b
Adam Bergman <adam@fransvilhelm.com>
2018-11-30 10:55:09 +0100

    fix: Update menu

--- main/menus.js
+++ main/menus.js
@@ -1,13 +1,22 @@
+'use strict';
+
 const { Menu } = require('electron');
+const { getIcon } = require('./utils/icons.js');
+const { productName, version } = require('../package.json');
 
 const trayMenuTemplate = [
+  {
+    icon: getIcon('trayIconTemplate.png'),
+    label: `${productName} v${version}`,
+    enabled: false,
+  },
+  {
+    type: 'separator',
+  },
   {
     label: 'Inställningar…',
     accelerator: 'Cmd+,',
     click: () => {},
-  },
-  {
-    type: 'separator',
   },
   {
     label: 'Avsluta Bryggan',
