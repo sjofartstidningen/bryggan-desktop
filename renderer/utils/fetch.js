477f22a261d3da922df7253c1aa1e13762810cb5
Adam Bergman <adam@fransvilhelm.com>
2018-12-10 11:21:45 +0100

    chore: Remove unused module

--- renderer/utils/fetch.js
+++
@@ -1,8 +0,0 @@
-const fetch = window && window.fetch ? window.fetch : require('node-fetch');
-
-const AbortController =
-  window && window.AbortController
-    ? window.AbortController
-    : require('abort-controller');
-
-export { fetch, AbortController };
