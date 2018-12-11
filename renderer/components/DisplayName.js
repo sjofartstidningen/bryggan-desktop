132fcbab4beb773c2af6c3d44d60f9329806d892
Adam Bergman <adam@fransvilhelm.com>
2018-12-11 08:21:53 +0100

    fix: Queue calls to getAccount to better utilize cache

--- renderer/components/DisplayName.js
+++ renderer/components/DisplayName.js
@@ -1,6 +1,14 @@
 import { CancelToken, isCancel } from 'axios';
 import { useState, useEffect, useContext } from 'react';
+import PQueue from 'p-queue';
 import { DropboxContext } from '../context/Dropbox';
+
+/**
+ * This queue is there to prevent fireing of simultaneous calls for the same
+ * account id – instead this will queue all calls and run them one by one and
+ * therefore the cached response will be available for subsequent calls.
+ */
+const queue = new PQueue({ concurrency: 1 });
 
 function DisplayName({ accountId }) {
   const [displayName, setDisplayName] = useState('');
@@ -9,11 +17,13 @@ function DisplayName({ accountId }) {
   useEffect(
     () => {
       const controller = CancelToken.source();
-      dropbox
-        .getAccount(accountId, {
-          cancelToken: controller.token,
-          ignoreCache: false,
-        })
+      queue
+        .add(() =>
+          dropbox.getAccount(accountId, {
+            cancelToken: controller.token,
+            ignoreCache: false,
+          }),
+        )
         .then(({ account }) => setDisplayName(account.displayName))
         .catch(error => {
           if (isCancel(error)) return;
