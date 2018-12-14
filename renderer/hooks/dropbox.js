3dc73aa0190c309a3df38fa731f4525197849767
Adam Bergman <adam@fransvilhelm.com>
2018-12-14 11:31:57 +0100

    fix: Create useListFolder hook

---
+++ renderer/hooks/dropbox.js
@@ -0,0 +1,66 @@
+import { CancelToken, isCancel } from 'axios';
+import { useReducer, useEffect } from 'react';
+import * as Dropbox from '../../shared/api/Dropbox';
+import { useIsMounted } from './';
+
+const ListFolderStage = {
+  initial: 'INITIAL',
+  fetching: 'FETCHING',
+  success: 'SUCCESS',
+  error: 'ERROR',
+};
+
+function useListFolder({ accessToken, initialPath }) {
+  const isMounted = useIsMounted();
+  const [state, setState] = useReducer(
+    (prevState, nextState) => ({ ...prevState, ...nextState }),
+    {
+      stage: ListFolderStage.initial,
+      currentPath: initialPath,
+      folderContent: [],
+      error: null,
+    },
+  );
+
+  const safeSetState = nextState => isMounted && setState(nextState);
+
+  const fetchCurrentPath = async ({ ignoreCache, cancelToken }) => {
+    try {
+      const { items } = await Dropbox.listFolder(state.currentPath, {
+        accessToken,
+        cancelToken,
+        ignoreCache,
+      });
+
+      safeSetState({
+        stage: ListFolderStage.success,
+        folderContent: items,
+        error: null,
+      });
+    } catch (error) {
+      if (!isCancel(error)) {
+        safeSetState({ stage: ListFolderStage.error, error });
+      } else {
+        safeSetState({ stage: ListFolderStage.success, error: null });
+      }
+    }
+  };
+
+  useEffect(
+    () => {
+      safeSetState({ stage: ListFolderStage.fetching });
+      const controller = CancelToken.source();
+      fetchCurrentPath({ cancelToken: controller.token, ignoreCache: false });
+      return () => controller.cancel();
+    },
+    [state.currentPath],
+  );
+
+  return {
+    ...state,
+    setPath: newPath => safeSetState({ currentPath: newPath }),
+    refreshCurrentPath: () => fetchCurrentPath({ ignoreCache: true }),
+  };
+}
+
+export { useListFolder, ListFolderStage };
---
+++ renderer/utils/__mocks__/ipc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  callMain: jest.fn(),
+  answerMain: jest.fn(),
+};
