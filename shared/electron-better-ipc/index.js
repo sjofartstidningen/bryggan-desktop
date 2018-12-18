219396d3ecaa14f8338cc339c6bb55521c893f48
Adam Bergman <adam@fransvilhelm.com>
2018-12-18 11:46:24 +0100

    fix: Patch electron-better-ipc
    Use patch from sindresorhus/electron-better-ipc #8 (by @npexxa93) until
    it merges since the ability to remove listeners is crucial in some
    places.

---
+++ shared/electron-better-ipc/index.js
@@ -0,0 +1,3 @@
+/* eslint-disable */
+'use strict';
+module.exports = require(process.type === 'renderer' ? './renderer' : './main');
---
+++ shared/electron-better-ipc/renderer.js
@@ -0,0 +1,54 @@
+/* eslint-disable */
+'use strict';
+const electron = require('electron');
+const util = require('./util');
+
+const { ipcRenderer: ipc } = electron;
+
+ipc.callMain = (channel, data) =>
+  new Promise((resolve, reject) => {
+    const { sendChannel, dataChannel, errorChannel } = util.getResponseChannels(
+      channel,
+    );
+
+    const cleanup = () => {
+      ipc.removeAllListeners(dataChannel);
+      ipc.removeAllListeners(errorChannel);
+    };
+
+    ipc.on(dataChannel, (event, result) => {
+      cleanup();
+      resolve(result);
+    });
+
+    ipc.on(errorChannel, (event, error) => {
+      cleanup();
+      reject(error);
+    });
+
+    ipc.send(sendChannel, data);
+  });
+
+ipc.answerMain = (channel, callback) => {
+  const window = electron.remote.getCurrentWindow();
+  const {
+    sendChannel,
+    dataChannel,
+    errorChannel,
+  } = util.getRendererResponseChannels(window.id, channel);
+
+  const listener = async (event, data) => {
+    try {
+      ipc.send(dataChannel, await callback(data));
+    } catch (error) {
+      ipc.send(errorChannel, error);
+    }
+  };
+
+  ipc.on(sendChannel, listener);
+  return () => {
+    ipc.removeListener(sendChannel, listener);
+  };
+};
+
+module.exports = ipc;
